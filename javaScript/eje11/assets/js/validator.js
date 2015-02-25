36c39bceeae7fd7e3cb91b05ea1c99400762b1fa
Unai <hermes752@gmail.com>
2015-02-25 19:24:22 +0100

    javascript

---
+++ javaScript/eje11/assets/js/validator.js
@@ -0,0 +1,29 @@
+HTMLFormElement.prototype.validate = function() {
+        var validate=function(){
+
+
+        };
+        this.addEventListener('submit', validate, false);
+
+
+};
+
+
+document.getElementById('login').validate();
+
+
+
+
+/*(function() {
+
+
+
+
+
+
+
+    
+});
+    
+
+*/
