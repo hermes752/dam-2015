d3d562d1d5f4031ab3ddbe1bd2b7104a1a4a827b
Unai <hermes752@gmail.com>
2015-03-09 19:18:35 +0100

    web worker

---
+++ html5/ej07/assets/js/app.js
@@ -0,0 +1,47 @@
+/*var botoi1 = document.getElementById("calcular");
+
+
+
+
+function getPrimes() {
+	var max= document.getElementById("number").value;
+    var sieve = [],
+        i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    console.log(primes);
+}
+
+if (botoi1) {
+        botoi1.addEventListener("click", getPrimes, false);
+    }
+*/
+window.onload=function(){
+var botoi1 = document.getElementById("calcular");
+var input=document.getElementById("number");
+var results=document.getElementById("results");
+
+var worker=new Worker('assets/js/primes.es');
+botoi1.addEventListener('click', function(e){
+
+e.preventDefault();
+worker.postMessage(num);
+
+results.innerHTML=primes.join(" ");
+
+});
+
+
+
+
+
+
+
+};
